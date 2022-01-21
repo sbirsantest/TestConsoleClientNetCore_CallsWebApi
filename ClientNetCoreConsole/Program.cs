f081999d40176f2ccf9b685e89e851fb10661959
sbarsan <sorinbarsan@gmail.com>
2022-01-21 23:31:52 +0200

    Add project files.

---
+++ ClientNetCoreConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace ClientNetCoreConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+            await host.RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services => 
+                services.AddHttpClient()
+                );
+ 
+    }
+}
