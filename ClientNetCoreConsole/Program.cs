d4b14abfb4b4b436413485d136a96fc460354170
sbarsan <sorinbarsan@gmail.com>
2022-01-21 23:47:43 +0200

    from the console client, call WeatherForecast API

--- ClientNetCoreConsole/Program.cs
+++ ClientNetCoreConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ClientNetCoreConsole
@@ -11,7 +12,13 @@ namespace ClientNetCoreConsole
         {
             using IHost host = CreateHostBuilder(args).Build();
 
-            await host.RunAsync();
+            using var scope = host.Services.CreateScope();
+            Task runHost = host.RunAsync();
+
+            using var http = scope.ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
+            await PrintWeatherForecastAsync(http);
+
+            await runHost;
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -19,6 +26,23 @@ namespace ClientNetCoreConsole
             .ConfigureServices(services => 
                 services.AddHttpClient()
                 );
- 
+
+        private static async Task PrintWeatherForecastAsync(HttpClient httpClient)
+        {
+            if (httpClient == null) throw new ArgumentNullException(nameof(httpClient));
+
+            try
+            {
+                var response = await httpClient.GetAsync($"http://localhost:5000/WeatherForecast");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
     }
 }
